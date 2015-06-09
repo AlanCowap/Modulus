b10e879e61f26d7386c19758b48b82c97bdefc67
Alan Cowap <spam@alancowap.com>
2015-06-09 12:11:27 +0100

    Add lots of functionality

--- Modulus/src/com/alancowap/java/modulus/ModulusGui.java
+++ Modulus/src/com/alancowap/java/modulus/ModulusGui.java
@@ -10,6 +10,7 @@
 package com.alancowap.java.modulus;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -18,8 +19,10 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JTextArea;
 
 public class ModulusGui extends JFrame {
@@ -32,6 +35,10 @@ public class ModulusGui extends JFrame {
 	JTextArea notModulusNumbers;
 	
 	private ModulusHandler handler = new ModulusHandler(this);
+	private JTextField denominator;
+	private JLabel lblDivisor;
+	private JLabel lblNumerator;
+	private JTextField txtError;
 	
 	
 	/**
@@ -77,24 +84,43 @@ public class ModulusGui extends JFrame {
 		num.setColumns(13);
 		
 		JButton btnCheck = new JButton("Check");
-		btnCheck.setBounds(162, 10, 89, 23);
+		btnCheck.setBounds(162, 42, 89, 23);
 		contentPane.add(btnCheck);
 		
-		JLabel lblModulus = new JLabel("Modulus 11");
-		lblModulus.setBounds(30, 52, 100, 14);
+		JLabel lblModulus = new JLabel("Modulus");
+		lblModulus.setBounds(30, 76, 100, 14);
 		contentPane.add(lblModulus);
 		
-		notModulus = new JLabel("Not Modulus 11");
-		notModulus.setBounds(162, 52, 100, 14);
+		notModulus = new JLabel("Not Modulus");
+		notModulus.setBounds(162, 76, 100, 14);
 		contentPane.add(notModulus);
 		
 		modulusNumbers = new JTextArea();
-		modulusNumbers.setBounds(30, 68, 100, 133);
+		modulusNumbers.setBounds(30, 92, 100, 133);
 		contentPane.add(modulusNumbers);
 		
 		notModulusNumbers = new JTextArea();
-		notModulusNumbers.setBounds(162, 68, 100, 133);
+		notModulusNumbers.setBounds(162, 92, 100, 133);
 		contentPane.add(notModulusNumbers);
+		
+		denominator = new JTextField();
+		denominator.setText("11");
+		denominator.setBounds(30, 42, 31, 20);
+		contentPane.add(denominator);
+		denominator.setColumns(10);
+		
+		lblDivisor = new JLabel("Denominator");
+		lblDivisor.setBounds(71, 42, 81, 14);
+		contentPane.add(lblDivisor);
+		
+		lblNumerator = new JLabel("Numerator");
+		lblNumerator.setBounds(140, 14, 67, 14);
+		contentPane.add(lblNumerator);
+		
+		txtError = new JTextField();
+		txtError.setBounds(30, 231, 232, 20);
+		contentPane.add(txtError);
+		txtError.setColumns(10);
 
 		// Adds actionlistener(s) to the button(s). 
 		btnCheck.addActionListener(handler);
@@ -103,15 +129,25 @@ public class ModulusGui extends JFrame {
 	
 	//Add some accessors and mutators
 	
-	public String getNum(){
+	public String getNumerator(){
 		return this.num.getText();
 	}
 	
-	public void addNotMod(String str){
-		this.notModulusNumbers.setText(this.notModulusNumbers.getText() + str + "\n");
+	public String getDenominator(){
+		return this.denominator.getText();
+	}
+	
+	public void addMod(String numerator, String denominator){
+		this.modulusNumbers.append(numerator +" ("+ denominator +")" + "\n");
 	}
 
-	public void addMod(String str){
-		this.notModulusNumbers.setText(this.modulusNumbers.getText() + str + "\n");
+	public void addNotMod(String numerator, String denominator){
+		this.notModulusNumbers.append(numerator +" ("+ denominator +")" + "\n");
 	}
+	
+	public void addError(String error){
+		this.txtError.setForeground(Color.RED);
+		this.txtError.setText(error);
+	}
+
 }
